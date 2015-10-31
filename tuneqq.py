6dc2c8f1c354767be787ca2c5b83c7ac9138bff0
Jason Chang <cchang5@illinois.edu>
2015-10-31 19:33:19 -0400

    finished updating tuneqq

--- tuneqq.py
+++ tuneqq.py
@@ -1,114 +1,93 @@
-#Fit for decay constants
+import sys
+sys.path.append('$HOME/c51/scripts/')
 import h5py as h5
 import c51lib as c51
 import numpy as np
 import gvar as gv
 import matplotlib.pyplot as plt
+from tabulate import tabulate
+import yaml
+import collections
 
 #Notes: for tuning fits, do single state no prior fit
 
-def read_data(filename, dpath_pion, dpath_kaon, plot='off'):
-    pion = c51.open_data(filename, dpath_pion)
-    kaon = c51.open_data(filename, dpath_kaon)
+def read_data(params, meson, plot_flag='off'):
+    ens = params['current_fit']['ens']
+    ml = params['current_fit']['ml']
+    ms = params['current_fit']['ms']
+    loc = params[ens][str(ml)+'_'+str(ms)]['data_loc']
+    # read data
+    twopt_dat = c51.fold(c51.open_data(loc['file_loc'], loc['twopt_'+meson]))
+    twopt_ss = np.squeeze(twopt_dat[:,:,0,:])
+    twopt_ps = np.squeeze(twopt_dat[:,:,1,:])
+    T = 2*len(twopt_ss[0])
+    if plot_flag == 'on':
+        # unfolded correlator data
+        c51.scatter_plot(np.arange(len(twopt_ss[0])), c51.make_gvars(twopt_ss), meson+' ss folded')
+        c51.scatter_plot(np.arange(len(twopt_ps[0])), c51.make_gvars(twopt_ps), meson+' ps folded')
+        # effective mass
+        eff = c51.effective_plots(T)
+        meff_ss = eff.effective_mass(c51.make_gvars(twopt_ss), 1, 'cosh')
+        meff_ps = eff.effective_mass(c51.make_gvars(twopt_ps), 1, 'cosh')
+        xlim = [3, len(meff_ss)]
+        ylim = c51.find_yrange(meff_ss, xlim[0], xlim[1])
+        c51.scatter_plot(np.arange(len(meff_ss)), meff_ss, meson+' ss effective mass', xlim = xlim, ylim = ylim)
+        ylim = c51.find_yrange(meff_ps, xlim[0], xlim[1])
+        c51.scatter_plot(np.arange(len(meff_ps)), meff_ps, meson+' ps effective mass', xlim = xlim, ylim = ylim)
+        # scaled correlator
+        E0 = dict()
+        E0['pion'] = 0.165
+        E0['kaon'] = 0.1
+        scaled_ss = eff.scaled_correlator(c51.make_gvars(twopt_ss), E0[meson], phase=1.0)
+        scaled_ps = eff.scaled_correlator(c51.make_gvars(twopt_ps), E0[meson], phase=1.0)
+        c51.scatter_plot(np.arange(len(scaled_ss)), scaled_ss, meson+' ss scaled correlator (take sqrt to get Z0_s)')
+        c51.scatter_plot(np.arange(len(scaled_ps)), scaled_ps, meson+' ps scaled correlator (divide by Z0_s to get Z0_p)')
+    return twopt_ss, twopt_ps, T
 
-    #Fold meson data
-    pion = c51.fold(pion)
-    kaon = c51.fold(kaon)
-
-    pion_gv = c51.make_gvars(pion)
-    kaon_gv = c51.make_gvars(kaon)
-
-    if plot=='on':
-        #Pion
-        dat = pion_gv
-        E0 = 0.13402
-        meff = c51.effective_mass(dat,1,'cosh')
-        x = np.arange(len(meff))
-        xlim = [2,len(meff)-2]
-        ylim = c51.find_yrange(meff, xlim[0], xlim[1])
-        c51.scatter_plot(x, meff, 'pion meff', xlim = xlim, ylim = ylim)
-        scaled2pt = c51.scaled_correlator(dat, E0)
-        ylim = c51.find_yrange(scaled2pt, xlim[0], xlim[1])
-        c51.scatter_plot(x, scaled2pt, 'pion scaled 2pt', xlim=xlim, ylim=ylim)
-        #ylim = c51.find_yrange(dat, xlim[0], xlim[1])
-        #c51.scatter_plot(x, dat, 'data', xlim=xlim, ylim=ylim)
-        #Kaon
-        dat = kaon_gv
-        E0 = 0.41559
-        meff = c51.effective_mass(dat,1,'cosh')
-        x = np.arange(len(meff))
-        ylim = c51.find_yrange(meff, xlim[0], xlim[1])
-        c51.scatter_plot(x, meff, 'kaon meff', xlim = xlim, ylim = ylim)
-        scaled2pt = c51.scaled_correlator(dat, E0)
-        ylim = c51.find_yrange(scaled2pt, xlim[0], xlim[1])
-        c51.scatter_plot(x, scaled2pt, 'kaon scaled 2pt', xlim=xlim, ylim=ylim)
-    else: pass
-
-    return pion_gv, kaon_gv
-
-def decay(pion_gv, kaon_gv):
-    prior = dict()
-    prior['Z0_s'] = gv.gvar(0.0, 10.0)
-    #prior['Z1_s'] = gv.gvar(0.06, 0.05)
-    #prior['Z2_s'] = gv.gvar(0.025, 0.035)
-    #prior['Z0_p'] = gv.gvar(0.27, 0.15)
-    #prior['Z1_p'] = gv.gvar(0.27, 0.35)
-    #prior['Z2_p'] = gv.gvar(0.27, 0.35)
-    prior['E0'] = gv.gvar(0.13, 10.0)
-    #prior['E1'] = gv.gvar(0.0, 1.0)
-    #prior['E2'] = gv.gvar(0.0, 1.0)
-    trange = dict()
-    trange['tmin'] = [5,5]
-    trange['tmax'] = [20,20]
-    T = len(pion_gv)*2
-    fitfcn = c51.fit_function(T=T, nstates=1)
-    fit = c51.fitscript(trange, pion_gv, prior, fitfcn.twopt_fitfcn_ss, sets=1, result_flag='on')
-    print "pion"
-    #c51.stability_plot(fit, 'Z0_p')
-    c51.stability_plot(fit, 'E0')
-    #ml = 0.0158
-    #ms = 0.0902
-    #mres_pi = gv.gvar(0.0009633, 0.0000065)
-    #Z0_p = fit['post'][0]['Z0_p']
-    #E0 = fit['post'][0]['E0']
-    #fpi = Z0_p*np.sqrt(2.)*(2.*ml+2.*mres_pi)/E0**(3./2.)
-    #print 'fpi:', fpi
-
-    print "kaon"
-    prior['Z0_s'] = gv.gvar(0.0, 10.0)
-    #prior['Z1_s'] = gv.gvar(0.037, 0.03)
-    #prior['Z2_s'] = gv.gvar(0.02, 0.03)
-    #prior['Z0_p'] = gv.gvar(0.2, 0.1)
-    #prior['Z1_p'] = gv.gvar(0.2, 0.3)
-    #prior['Z2_p'] = gv.gvar(0.2, 0.3)
-    prior['E0'] = gv.gvar(0.415, 10.0)
-    #prior['E1'] = gv.gvar(0.0, 1.0)
-    #prior['E2'] = gv.gvar(0.0, 1.0)
-    trange['tmin'] = [5,5]
-    trange['tmax'] = [20,20]
-    fitfcn = c51.fit_function(T=T, nstates=1)
-    fit = c51.fitscript(trange, kaon_gv, prior, fitfcn.twopt_fitfcn_ss, sets=1, result_flag='on')
-    #c51.stability_plot(fit, 'Z0_p')
-    c51.stability_plot(fit, 'E0')
-    #mres_kaon = gv.gvar(0.0006685, 0.0000044)
-    #Z0_p = fit['post'][0]['Z0_p']
-    #E0 = fit['post'][0]['E0']
-    #fk = Z0_p*np.sqrt(2.)*(ml+ms+mres_pi+mres_kaon)/E0**(3./2.)
-    #print 'fk:', fk
-    #fkfpi = fk/fpi
-    #print 'fk/fpi:', fkfpi
+def fittwo_pt(params, meson, twopt_ss, twopt_ps, T):
+    ens = params['current_fit']['ens']
+    ml = params['current_fit']['ml']
+    ms = params['current_fit']['ms']
+    # make data
+    twopt_gv = c51.make_gvars(np.concatenate((twopt_ss, twopt_ps), axis=1))
+    # priors
+    priors = params[ens][str(ml)+'_'+str(ms)]['priors'][meson]
+    priors_gv = c51.dict_of_tuple_to_gvar(priors)
+    # read trange
+    trange = params[ens][str(ml)+'_'+str(ms)]['trange']
+    # fit
+    fitfcn = c51.fit_function(T, nstates=1)
+    fit = c51.fitscript_v2(trange, T, twopt_gv, priors_gv, fitfcn.twopt_fitfcn_ss_ps, result_flag='off')
+    return np.array([[0, fit]])
 
 if __name__=='__main__':
-    #filename = '/home/cchang5/c51/data/l3264f211b600m00507m0507m628a_avg.h5'
-    #dpath_pion = 'l3264f211b600m00507m0507m628/wf1p0_m51p2_l512_a52p0_smrw5p5_n75/phi_qq/mq0p00600/corr'
-    #dpath_kaon = 'l3264f211b600m00507m0507m628/wf1p0_m51p2_l512_a51p8_smrw5p5_n75/phi_qq/mq0p0693/corr'
-    filename = '/home/cchang5/c51/data/l3248f211b580m00235m0647m831a_avg.h5'
-    dpath_pion = 'l3248f211b580m00235m0647m831a/wf1p0_m51p3_l524_a53p5_smrw4p5_n60/phi_qq/mq0p00211/corr'
-    dpath_kaon = 'l3248f211b580m00235m0647m831a/wf1p0_m51p3_l524_a53p5_smrw4p5_n60/phi_qq/mq0p00211/corr'
-    #filename = '/Users/cchang5/c51/data/l2464f211b600m0102m0509m635a_avg.h5'
-    #dpath_pion = 'l2464f211b600m0102m0509m635/wf1p0_m51p2_l58_a51p5_smrw5p0_n75/spectrum/ml0p0126_ms0p0693/pion/corr'
-    #dpath_kaon = 'l2464f211b600m0102m0509m635/wf1p0_m51p2_l58_a51p5_smrw5p0_n75/spectrum/ml0p0126_ms0p0693/kaon/corr'
-    pion, kaon = read_data(filename, dpath_pion, dpath_kaon, 'on')
-    decay(pion, kaon)
-
+    # switches
+    meson = 'pion'
+    effective_plot_flag = 'off'
+    stability_plot_flag = 'on'
+    fit_tbl_flag = 'on'
+    # read parameters
+    f = open('./tuneqq.yml','r')
+    params = yaml.load(f)
+    f.close()
+    # read data
+    twopt_ss, twopt_ps, T = read_data(params, meson, plot_flag=effective_plot_flag)
+    # fit two point
+    fit = fittwo_pt(params, meson, twopt_ss, twopt_ps, T)
+    # process fit
+    fit_proc = c51.process_bootstrap(fit)
+    fit_boot0, fit_bs = fit_proc()
+    if stability_plot_flag == 'on':
+        c51.stability_plot(fit_boot0, 'Z0_s', meson+' Z0_s ')
+    if fit_tbl_flag == 'on':
+        tbl = collections.OrderedDict()
+        tbl['tmin'] = fit_proc.tmin
+        tbl['tmax'] = fit_proc.tmax
+        tbl['Z0_s'] = fit_proc.read_boot0('Z0_s')
+        tbl['Z0_s err'] = fit_proc.read_boot0_sdev('Z0_s')
+        tbl['Z0_p'] = fit_proc.read_boot0('Z0_p')
+        tbl['Z0_p err'] = fit_proc.read_boot0_sdev('Z0_p')
+        tbl['E0'] = fit_proc.read_boot0('E0')
+        tbl['E0 err'] = fit_proc.read_boot0_sdev('E0')
+        print tabulate(tbl, headers='keys')
     plt.show()
