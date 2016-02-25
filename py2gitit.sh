d3ad548bac4fd2ef41e19dce6e6cd03127a71609
Jason Chang <cchang5@illinois.edu>
2016-02-24 16:59:58 -0800

    update py2gitit

--- py2gitit.sh
+++ py2gitit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # remove leading white spaces, trade whitespace for column separator |, add | to end of line
-sed -e 's/^[[:space:]]\+//g' -e 's/[[:space:]]\+/|/g' -e 's/$/|/g'
+sed -e 's/^[[:space:]]\+//g' -e 's/[[:space:]][[:space:]]*/|/g' -e 's/$/|/g'
